ae250f8d76fb4d92b44bd2714f705a6302af1295
Vicka <vicrogatinskaya@gmail.com>
2023-08-10 21:58:46 +0200

    more

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+
+</script>
+
+<template>
+  <div>
+
+    <h1>Hi!</h1>
+    <h1>My name is Victoria</h1>
+
+    <h2>
+        I'm a front-end developer with a passion for <br/>
+        programming, art and games.
+    </h2>
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/art.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Art</h1>
+    </div>
+</template>
+
+<script>
+</script>
