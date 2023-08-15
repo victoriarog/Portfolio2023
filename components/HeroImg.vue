a32fc03b4dd6d73e9c425f4cec2edcd4c0fd4754
Vicka <vicrogatinskaya@gmail.com>
2023-08-16 01:33:35 +0200

    hero img

---
+++ components/HeroImg.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="indexhero">
+        <img class="heroimg" src="../assets/heros/hero.png"/>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template> 
     <Header/>
     <IntroT/>
+    <HeroImg/>
 </template>
