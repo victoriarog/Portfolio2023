542462cd2aa6554e247b5ee3655b34a54970438a
Vicka <vicrogatinskaya@gmail.com>
2023-08-28 02:14:16 +0200

    removed unused

--- components/HeroImg.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="indexhero">
-        <img class="backimg" src="../assets/heros/back.png"/>
-        <Arrows/>
-        <img class="girlimg" src="../assets/heros/girl.png"/>
-    </div>
-</template>
